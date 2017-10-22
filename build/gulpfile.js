54876e1da089fb50e48a7a564715e84b528c1c7f
pixelbits <pixelbits100@gmail.com>
2017-10-22 04:42:11 -0600

    updated

--- build/gulpfile.js
+++ build/gulpfile.js
@@ -13,18 +13,12 @@ gulp.task('clean', function () {
     ], { force: true });
 });
 
-gulp.task('pre-deploy', function () {
-    return del([
-        "../dist/**"
-    ], { force: true });
-});
-
 
 gulp.task('copy-public-api', ['clean'], function () {
     return gulp.src([
         'public_api.ts'
     ])
-        .pipe(gulp.dest('dist'))
+    .pipe(gulp.dest('dist'))
 
 });
 gulp.task('copy-src', ['copy-public-api'], function () {
@@ -36,9 +30,14 @@ gulp.task('copy-src', ['copy-public-api'], function () {
         .pipe(gulp.dest('dist/src'))
 });
 
-gulp.task('compile', ['copy-src'], function () {
-    return gulp.src('tsconfig.json')
-        .pipe(shell(['"../node_modules/.bin/ngc" -p <%= file.path %>']));
+gulp.task('compile', ['copy-src'], function (done) {
+     gulp.src('tsconfig.json')
+        .pipe(shell(['"../node_modules/.bin/ngc" -p <%= file.path %>']))
+        .on('end', function() {
+            del('node_modules/**', { force: true}).then(function() {
+                done();
+            });
+        });
 });
 
 gulp.task('bundle', ['compile'], function () {
@@ -115,44 +114,50 @@ gulp.task('bundle', ['compile'], function () {
     });
 });
 
-gulp.task('build', ['bundle'], function () {
+gulp.task('pre-build', function () {
     return del([
-        "node_modules/**"
+        "../dist/**"
     ], { force: true });
 });
 
-gulp.task('deploy', ['build', 'pre-deploy'], function () {
-    return gulp.src([
+
+gulp.task('build', ['bundle', 'pre-build'], function (done) {
+    gulp.src([
         'dist/index.es5.js',
         'dist/public_api.js',
         'dist/index.metadata.json',
         'dist/bundles/**.js',
         'dist/**/*.d.ts',
         '!../src/app/**/*.spec.ts'
-    ], { base: 'dist' })
+        ], { base: 'dist' })
         .pipe(gulp.dest('../dist'))
+        .on('end', function() {
+            del('dist/**', { force: true}).then(function() {
+                done();
+            });
+        });
 });
 
-gulp.task('publish', [], function () {
-    var tasks = [];
+gulp.task('pre-publish', function() {
     if (process.argv.length == 4) {
-        var dest = process.argv[3].substring(2);
-        var copyTask = gulp.src([
+        var destPath = process.argv[3].substring(2);
+        var packagePath = path.resolve(destPath, pkg.name);
+        return del(packagePath, { force:true});
+    }
+});
+
+gulp.task('publish', ['pre-publish'], function () {
+    if (process.argv.length == 4) {
+        var destPath = process.argv[3].substring(2);
+        var packagePath = path.resolve(destPath, pkg.name);
+
+        return gulp.src([
             '../package.json',
             '../dist/**/*'
         ], { base: '../'})
-        .pipe(gulp.dest(path.resolve(dest, pkg.name)));
-        tasks.push(copyTask);     
+        .pipe(gulp.dest(packagePath));  
     }
     
-    
-    var delTask = del([
-        "dist/**"
-    ], { force: true });
-
-    tasks.push(delTask);
-    return tasks;
-
 });
 
 gulp.task('name-module', function () {

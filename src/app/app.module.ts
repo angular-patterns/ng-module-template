f85cc97c8a040052d44c00adde0d5507ed24d0b6
pixelbits <pixelbits100@gmail.com>
2017-10-24 02:53:57 -0600

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }  from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { MessageComponent } from './message/message.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports:      [ BrowserModule, HttpClientModule ],
+  declarations: [ AppComponent,MessageComponent ],
   bootstrap:    [ AppComponent ],
-  exports: [AppComponent]
+  exports: [AppComponent, MessageComponent]
 })
 export class AppModule { }
---
+++ src/app/message/message.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Message } from './message.model';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/timer';
+import 'rxjs/add/observable/combineLatest';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/takeWhile';
+
+@Component({
+    selector: 'message',
+    templateUrl: 'message.component.html'
+})
+
+export class MessageComponent implements OnInit {
+    messages: Observable<Message[]>;
+    timer: Observable<number>;
+    rotating: Observable<Message[]>;
+
+    constructor(@Inject(HttpClient) private http: HttpClient) { 
+        this.messages = Observable.timer(0,10000).switchMap(t=> this.http.get<Message[]>(`${process.env.MESSAGE_URL}`));
+        this.timer = Observable.timer(0,3000);
+
+        this.rotating = Observable.combineLatest(this.messages, this.timer).map(arr=> {
+            var messages = arr[0];
+            if (messages.length <= 5)
+                return messages;
+
+            var index = arr[1] % messages.length;
+            var sliced = messages.slice(index);
+            for (var i = 0; i < index; ++i) {
+                sliced.push(messages[i]);
+            }
+            return sliced;
+
+        }).map(t=> {
+            if (t.length <= 5)
+                return t;
+            else  
+                return t.slice(0, 5);
+        });
+
+
+       
+    }
+
+    ngOnInit() { }
+}

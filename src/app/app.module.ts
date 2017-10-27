7c90bc207be5d7723239740197459e0e69fdf946
pixelbits <pixelbits100@gmail.com>
2017-10-27 14:18:12 -0600

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,16 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }  from './app.component';
+import { AppModule as TimeModule, TimeComponent } from 'time';
+import { WidgetComponent } from './widget/widget.component';
+import { WidgetFactory } from './widget/widget.factory';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports:      [ BrowserModule,TimeModule ],
+  declarations: [ AppComponent, WidgetComponent ],
   bootstrap:    [ AppComponent ],
-  exports: [AppComponent]
+  exports: [AppComponent],
+  entryComponents: [TimeComponent],
+  providers: [WidgetFactory]
 })
 export class AppModule { }

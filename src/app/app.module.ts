18c0af2e1d999868586fdb07a257e9ea51d86fc1
pixelbits <pixelbits100@gmail.com>
2017-10-28 22:18:57 -0600

    udpated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }  from './app.component';
+import { InboxWidgetComponent } from "./widget/inbox-widget.component";
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, InboxWidgetComponent ],
   bootstrap:    [ AppComponent ],
   exports: [AppComponent]
 })
---
+++ src/app/widget/inbox-widget.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'inbox-widget',
+    templateUrl: 'inbox-widget.component.html',
+    styleUrls: [
+        'inbox-widget.component.css'
+    ]
+})
+
+export class InboxWidgetComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}

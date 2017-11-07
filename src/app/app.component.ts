56a38b41011eef9edce900caed01e86d94873242
mike <mike@stuff.com>
2017-11-07 09:25:42 -0700

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,18 +9,32 @@ import { MessageService } from "@portal/core";
 export class AppComponent implements OnInit {
     constructor(private messageService:MessageService) {
         //this.messageService.changePortal('three-column-template');
+        // this.messageService.pushWidgets([
+        //     { widget: 'inbox-widget', location: 1 },
+        //     { widget: 'inquiry-widget', location: 1 },
+        //     { widget: 'time-widget', location: 2 },
+        //     { widget: 'app-widget', location: 2 },
+        //     { widget: 'scholarship-widget', location: 2 },
+        //     { widget: 'rfr-widget', location: 2 },
+        //     { widget: 'edoc-widget', location: 3 },
+        //     { widget: 'address-widget', location: 3 },
+        //     { widget: 'weather-widget', location: 5 },
+        //     { widget: 'address-lookup-widget', location: 4 },
+
+            
+        // ]);
+
         this.messageService.pushWidgets([
+            // { widget: 'time-widget', location: 1 },
             { widget: 'inbox-widget', location: 1 },
             { widget: 'inquiry-widget', location: 1 },
-            { widget: 'time-widget', location: 2 },
-            { widget: 'app-widget', location: 2 },
-            { widget: 'scholarship-widget', location: 2 },
-            { widget: 'rfr-widget', location: 2 },
-            { widget: 'edoc-widget', location: 3 },
-            { widget: 'address-widget', location: 3 },
-            { widget: 'weather-widget', location: 5 },
-            { widget: 'address-lookup-widget', location: 4 },
-
+            { widget: 'app-widget', location: 1 },
+            { widget: 'scholarship-widget', location: 1 },
+            { widget: 'rfr-widget', location: 1 },
+            { widget: 'edoc-widget', location: 1 },
+            { widget: 'address-widget', location: 1 },
+            //{ widget: 'weather-widget', location: 1 },
+            //{ widget: 'address-lookup-widget', location: 1 },
             
         ]);
      }

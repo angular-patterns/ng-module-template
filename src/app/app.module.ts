40889fb5b5fbfe16b7e0b608e7562dc782404248
pixelbits <pixelbits100@gmail.com>
2017-11-07 07:52:36 -0700

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { 
     PortalSfsModule, 
     DefaultTemplateComponent, 
@@ -15,13 +17,15 @@ import {
     WeatherWidgetComponent,
     AddressLookupWidgetComponent,
     TimeWidgetComponent
+    
 } 
 from "./portal-sfs/portal-sfs.module";
 
 @NgModule({
     imports:      [ 
         BrowserModule, 
-        PortalSfsModule
+        PortalSfsModule,
+        BrowserAnimationsModule
     ],
     declarations: [ 
         AppComponent
--- src/app/portal-ui/common/widget/widget.component.ts
+++ src/app/portal-ui/common/widget/widget.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, AfterViewInit } from '@angular/core';
 import { Widget } from "./widget.model";
+import { trigger,state,style,transition,animate,keyframes } from '@angular/animations';
 
 @Component({
     selector: 'ui-widget',
     templateUrl: 'widget.component.html',
-    styleUrls: ['widget.component.css']
+    styleUrls: ['widget.component.css'],
+    animations: [
+        trigger('myanimation', [
+            state('fadein', style({
+                opacity: 0
+            })),
+            state('fadeout', style({
+                opacity: 1
+            })),
+            transition('fadein <=> fadeout', animate('1000ms ease-in'))
+        ])
+    ]
 })
 
-export class WidgetComponent implements OnInit {
-    @Input() model:Widget;
+export class WidgetComponent implements OnInit, AfterViewInit {
+    @Input() model: Widget;
+    state: string = 'fadein';
+
     constructor() { }
+
+    animateMe() {
+        this.state = this.state == "fadein" ? "fadeout": "fadein";
+    }
+    ngAfterViewInit(): void {
+        setTimeout(t=> {
+            this.animateMe();
+        },0);
+    }
 
     ngOnInit() { }
 }

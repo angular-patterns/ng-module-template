355b21f33ed4cbab1f3c86f0a7e547acdfff1f1f
pixelbits <pixelbits100@gmail.com>
2017-11-15 01:14:25 -0700

    finally fixed ng5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,9 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }  from './app.component';
-import { TestModule } from 'test';
 
 @NgModule({
-  imports:      [ BrowserModule, TestModule ],
+  imports:      [ BrowserModule],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ],
   exports: [AppComponent]

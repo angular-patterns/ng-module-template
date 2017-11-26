da4256f57b14a6014a7a5d971c8eae0b1482cdbf
pixelbits <pixelbits100@gmail.com>
2017-11-26 04:43:55 -0700

    added tslint

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
-      
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent }  from './app.component';
+import { AppComponent } from './app.component';
 
 @NgModule({
   imports:      [ BrowserModule],
@@ -9,4 +8,5 @@ import { AppComponent }  from './app.component';
   bootstrap:    [ AppComponent ],
   exports: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}

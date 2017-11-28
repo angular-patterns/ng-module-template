07d95bdc791d37f83979dd5a63636cd97ad92d05
pixelbits <pixelbits100@gmail.com>
2017-11-27 21:46:55 -0700

    fixed lint issues

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -4,23 +4,18 @@ export class AppPage {
   navigateTo() {
     return browser.get('/');
   }
-
   getParagraphText() {
     return element(by.css('app-root h1')).getText();
   }
 }
 
-
 describe('App', () => {
     let page: AppPage;
-  
     beforeEach(() => {
       page = new AppPage();
     });
-  
     it('should display welcome message', () => {
       page.navigateTo();
       expect(page.getParagraphText()).toEqual('Hello World!');
     });
   });
-  

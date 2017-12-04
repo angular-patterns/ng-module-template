9fef6932412bb72ef122a54421f2e424f5bc262d
Michael.Kang <michael.kang@gov.ab.ca>
2017-12-03 23:09:28 -0700

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ModalModule } from 'ng-modal-dialog';
 import { TestComponent } from './test/test.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes:Routes = [ 
+{ path: 'test', component: TestComponent, outlet: 'modal'}
+];
 
 @NgModule({
   imports:      [ 
       BrowserModule,
-      ModalModule.forRoot([
-          { name: 'test', component: TestComponent }
-      ])
+      ModalModule,
+      RouterModule.forRoot(routes)
     ],
   declarations: [ AppComponent, TestComponent ],
   bootstrap:    [ AppComponent ],

b149d8c4f7bc32e28d7b48fcb8507cc3a996d195
Michael.Kang <michael.kang@gov.ab.ca>
2017-12-04 20:20:32 -0700

    updateed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { TestModule } from '../test/test.module';
 
 @NgModule({
-  imports:      [ BrowserModule],
+  imports:      [ BrowserModule, TestModule],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ],
   exports: [AppComponent]

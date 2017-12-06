c81787ec00f0cb7136d83cbfa2b6e8bdfe2e5462
Michael.Kang <michael.kang@gov.ab.ca>
2017-12-05 21:53:17 -0700

    Updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { FeatureModule } from 'feature';
+import { FeatureModule } from '../feature/feature.module';
 
 @NgModule({
   imports:      [ BrowserModule, FeatureModule],

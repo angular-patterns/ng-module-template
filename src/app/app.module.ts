3d8a9ec2c83c312b3fc399d22a12c868d3c9223e
Michael.Kang <michael.kang@gov.ab.ca>
2017-12-08 21:56:31 -0700

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { FeatureModule } from '../feature/feature.module';
-import { ModalModule } from 'modal';
+import { FeatureModule } from 'modal';
 import { TestComponent } from './test.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports:      [ BrowserModule, FeatureModule, ModalModule.forRoot([{ path: 'test', component: TestComponent, outlet: 'modal' }])],
+  imports:      [ 
+    BrowserModule, 
+    RouterModule.forRoot([]), 
+    FeatureModule.forRoot([
+      { name: 'test', component: TestComponent }
+    ])
+  ],
   declarations: [ AppComponent, TestComponent ],
   bootstrap:    [ AppComponent ],
   exports: [AppComponent]

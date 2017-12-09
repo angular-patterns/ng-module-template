d5e3d5bd6afae8f749b07e19e67b1e4db41bb45e
Michael.Kang <michael.kang@gov.ab.ca>
2017-12-08 21:36:36 -0700

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FeatureModule } from '../feature/feature.module';
+import { ModalModule } from 'modal';
+import { TestComponent } from './test.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FeatureModule],
-  declarations: [ AppComponent ],
+  imports:      [ BrowserModule, FeatureModule, ModalModule.forRoot([{ path: 'test', component: TestComponent, outlet: 'modal' }])],
+  declarations: [ AppComponent, TestComponent ],
   bootstrap:    [ AppComponent ],
   exports: [AppComponent]
 })
---
+++ src/app/test.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'test',
+    template: `
+        Hello World
+    `
+})
+
+export class TestComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}

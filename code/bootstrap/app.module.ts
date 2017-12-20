e531613944398d580b6d525f26103a3ea45546b8
Michael.Kang <michael.kang@gov.ab.ca>
2017-12-20 02:12:35 -0700

    updated

--- code/bootstrap/app.module.ts
+++ code/bootstrap/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
--- src/app/code.ts
+++ src/app/code.ts
@@ -1,8 +1,8 @@
 export const code = {
-    main: { language: 'typescript', content: require('!!raw-loader!../../code/main.ts') },
-    appmodule: { language: 'typescript', content: require('!!raw-loader!../../code/app.module.ts') },
-    appcomponent: { language: 'typescript', content: require('!!raw-loader!../../code/app.component.ts') },
-    appcomponenthtml: { language: 'html', content: require('!!raw-loader!../../code/app.component.html') },
-    indexhtml: { language: 'html', content: require('!!raw-loader!../../code/index.html') },
+    main: { language: 'typescript', content: require('!!raw-loader!../../code/bootstrap/main.ts') },
+    appmodule: { language: 'typescript', content: require('!!raw-loader!../../code/bootstrap/app.module.ts') },
+    appcomponent: { language: 'typescript', content: require('!!raw-loader!../../code/bootstrap/app.component.ts') },
+    appcomponenthtml: { language: 'html', content: require('!!raw-loader!../../code/bootstrap/app.component.html') },
+    indexhtml: { language: 'html', content: require('!!raw-loader!../../code/bootstrap/index.html') },
     
 }

ecff8fc964c6f0789101a5683c237e4eb607d157
Michael.Kang <michael.kang@gov.ab.ca>
2018-01-11 01:18:23 -0700

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
+import { PortalService } from '@sa/portal';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'sa-app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title: string;
-  constructor() {
+  constructor(private portalService: PortalService) {
     this.title = 'Hello World!';
+
   }
+
+  go() {
+   
+    this.portalService.push('1', ['widget', 'widget']);
+
+  }
+
+  ngOnInit() {
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FeatureModule } from '../feature/feature.module';
+import { CoreModule, CommonModule } from '@sa/portal';
+import { WidgetComponent } from '../feature/widget.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FeatureModule],
+  imports:      [ 
+    BrowserModule, 
+    FeatureModule,
+    CoreModule.forRoot([
+       { key: 'widget', component: WidgetComponent}
+    ]),
+    CommonModule
+  ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ],
   exports: [AppComponent]

1d5a1195033bfe15b34c80a24e4d16f3ea954628
Michael.Kang <michael.kang@gov.ab.ca>
2018-01-24 09:01:49 -0700

    updated

--- src/app/error-handler/error-handler.module.ts
+++ src/app/error-handler/error-handler.module.ts
@@ -2,6 +2,8 @@ import { NgModule, ErrorHandler, InjectionToken } from '@angular/core';
 import { FormatterFactory } from './formatter.factory';
 import { Formatter } from './shared/formatter';
 import { ERROR_FORMATTERS } from './index';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { RetryInterceptor } from './retry.interceptor';
 
 export const FormatterToken = new InjectionToken<Formatter[]>("Error Formatters");
 
@@ -16,6 +18,12 @@ export class Test {
     providers: [
         ERROR_FORMATTERS,
         { provide: FormatterFactory, useFactory: (...formatters)=> new FormatterFactory(formatters), deps: ERROR_FORMATTERS},
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: RetryInterceptor,
+            multi: true,
+        }
+  
     ]
 })
 export class ErrorHandlerModule { 

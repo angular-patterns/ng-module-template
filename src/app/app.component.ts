57d12f68f009b5febfd7c5d3b70758f12714a0ac
Michael.Kang <michael.kang@gov.ab.ca>
2018-01-24 08:49:39 -0700

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,20 @@ import { Component, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/mergeMap';
+import 'rxjs/add/operator/switchMap';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/finally';
 import 'rxjs/add/observable/forkJoin';
 import 'rxjs/add/observable/interval';
+import 'rxjs/add/operator/publish';
+import 'rxjs/add/operator/merge';
+import 'rxjs/add/observable/merge';
+import 'rxjs/add/operator/startWith';
 
 import { ErrorModel } from './error-handler/shared/error.model';
 import { GlobalErrorHandler } from './core/global-error.handler';
 import { ErrorHandler } from '@angular/core';
+import { Subject } from 'rxjs/Subject';
 
 @Component({
   selector: 'sa-app-root',
@@ -19,29 +25,29 @@ import { ErrorHandler } from '@angular/core';
 export class AppComponent {
   title: string;
   errors: Observable<ErrorModel[]>;
+  clearEvent: Subject<ErrorModel[]>;
   constructor(private http:HttpClient, @Inject(ErrorHandler) private errorHandler: GlobalErrorHandler) {
-    this.refresh();
-    this.errorHandler.lastError.subscribe(t=> {
-      this.refresh();
-    });
+    this.clearEvent = new Subject<ErrorModel[]>();
+    this.errors =  Observable.merge(
+        this.getErrors(),
+        this.errorHandler.lastError.switchMap(t=>this.getErrors()),
+        this.clearEvent
+           .switchMap(t=> this.getErrors())
+           .mergeMap(t=> Observable.forkJoin(t.map(x=>this.http.delete(`http://localhost:3000/errors/${x.id}`))))
+           .map(t=>[])   
+        );
   }
-  error1() {
+  simulateError1() {
     throw new Error("this is a general error");
   }
-  error2() {
+  simulateError2() {
     this.http.get('http://urldoesnotexist').subscribe();
   }
   getErrors() {
     return this.http.get<ErrorModel[]>('http://localhost:3000/errors?_sort=id&_order=desc');     
   }
-  refresh() {
-    this.errors = this.getErrors();
-  }
   clear() {
-    this.errors = 
-      this.getErrors()
-      .mergeMap(t=> Observable.forkJoin(t.map(x=>this.http.delete(`http://localhost:3000/errors/${x.id}`))))
-      .map(t=> []);
+    this.clearEvent.next();
   }
 
 

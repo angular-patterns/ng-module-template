00a897a91678f26cea85672c460cd2b9e96f742a
Michael.Kang <michael.kang@gov.ab.ca>
2018-01-24 21:03:59 -0700

    updated

--- src/app/core/ui/error-dialog/error-dialog.component.ts
+++ src/app/core/ui/error-dialog/error-dialog.component.ts
@@ -2,6 +2,8 @@ import { OnInit, Component, Input } from "@angular/core";
 import { BsModalRef } from "ngx-bootstrap/modal";
 import { ErrorInfo } from "../../../error/index";
 
+const production = process.env.Environment === 'Production';
+
 @Component({
     selector: 'modal-content',
     templateUrl:'error-dialog.component.html'
@@ -9,7 +11,10 @@ import { ErrorInfo } from "../../../error/index";
 
 export class ErrorDialogComponent implements OnInit {
     @Input() error: ErrorInfo;
-    constructor(public bsModalRef: BsModalRef) {}
+    production: boolean;
+    constructor(public bsModalRef: BsModalRef) {
+        this.production = production;
+    }
     ngOnInit() {
         
      }

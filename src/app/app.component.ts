247af225e8432e02f4d6a644050785af2afa248f
mike <mike@stuff.com>
2018-02-08 15:04:49 -0700

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Todo } from './todo.model';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
   title: string;
   todos: Todo[];
+  sub1: Subcription;
+  sub2: Subcription;
   constructor(private http: HttpClient) {
     this.title = 'Hello World!';
     this.todos = [];
-    this.http.get<Todo[]>('http://localhost:3000/todos').subscribe(t => {
+    this.sub1 = this.http.get<Todo[]>('http://localhost:3000/todos').subscribe(t => {
       this.todos = t;
     })
   }
 
   add(name: string) {
-     this.http.post<Todo>('http://localhost:3000/todos', { id: 0, name: name}).subscribe(t=> {
+     this.sub2 = this.http.post<Todo>('http://localhost:3000/todos', { id: 0, name: name}).subscribe(t=> {
        this.todos.push(t);
      });
   }
+
+  ngOnDestroy() {
+    this.sub1.unsubscribe();
+    this.sub2.unsubscribe();
+  }
+
 }

b631eaf85362843ab535818cfcf3ac2ce9c7f366
mike <mike@stuff.com>
2018-02-08 15:50:10 -0700

    updated

--- public_api.ts
+++ public_api.ts
@@ -5,5 +5,5 @@
  * Entry point for all public APIs of the core package.
  */
 
-export * from './src/app/app.module';
+export * from './src/app/time/time.module';
 export * from './src/app/time/time.component';
---
+++ src/app/time/time.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+import { TimeComponent } from './time.component';
+
+@NgModule({
+    imports: [],
+    exports: [ TimeComponent ],
+    declarations: [TimeComponent],
+    providers: [],
+})
+export class TimeModule { }

81914321d5dd0b7a143d39a656b710de3f6e98f4
mike <mike@stuff.com>
2018-02-08 15:00:47 -0700

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Todo } from './todo.model';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +9,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title: string;
-  constructor() {
+  todos: Todo[];
+  constructor(private http: HttpClient) {
     this.title = 'Hello World!';
+    this.todos = [];
+    this.http.get<Todo[]>('http://localhost:3000/todos').subscribe(t => {
+      this.todos = t;
+    })
+  }
+
+  add(name: string) {
+     this.http.post<Todo>('http://localhost:3000/todos', { id: 0, name: name}).subscribe(t=> {
+       this.todos.push(t);
+     });
   }
 }

a2c2b6adca7b5aebb14d63dcf97a68ddfcb63be4
Michael.Kang <michael.kang@gov.ab.ca>
2018-05-15 02:55:05 -0600

    updated

--- src/app/add-todo/add-todo.component.spec.ts
+++ src/app/add-todo/add-todo.component.spec.ts
@@ -3,12 +3,22 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { AddTodoComponent } from './add-todo.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ModalModule } from 'ngx-bootstrap/modal';
+import { defer } from 'rxjs';
+import { TodoService } from '../core/todo.service';
+import { By } from '@angular/platform-browser';
+import { BsModalService } from 'ngx-bootstrap/modal/bs-modal.service';
+
+export function asyncData<T>(data: T) {
+  return defer(() => Promise.resolve(data));
+}
 
 describe('AddTodoComponent', () => {
   let component: AddTodoComponent;
   let fixture: ComponentFixture<AddTodoComponent>;
 
   beforeEach(async(() => {
+    const todoService =  jasmine.createSpyObj('TodoService', ['load']);
+    const modalService = jasmine.createSpyObj('BsModalService', ['openModal'] );
     TestBed.configureTestingModule({
       imports: [
         FormsModule,
@@ -16,7 +26,11 @@ describe('AddTodoComponent', () => {
         ModalModule.forRoot()
       
       ],
-      declarations: [ AddTodoComponent ]
+      declarations: [ AddTodoComponent ],
+      providers: [
+        { provide: TodoService, useValue: todoService },
+        { provide: BsModalService, useValue: modalService }
+      ]
     })
     .compileComponents();
   }));
@@ -30,4 +44,26 @@ describe('AddTodoComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  it ('should init form', () => {
+    expect(component.formGroup).toBeTruthy();
+
+    const control = component.formGroup.get('title');
+    expect(control).toBeTruthy();
+    expect(control.value).toEqual('');
+    expect(control.hasError('required')).toBe(true);
+  })
+
+  it ('should clear form before opening modal', () => {
+    const de = fixture.debugElement;
+    const btn = de.query(By.css('button[type="button"]'));
+    expect(btn).toBeTruthy();
+
+    //const control = component.formGroup.get('title');
+    
+    btn.triggerEventHandler('click', null);
+    //expect(control).toBeTruthy();
+   // expect(control.value).toEqual('');
+   // expect(control.hasError('required')).toBe(true);
+  })
+
 });

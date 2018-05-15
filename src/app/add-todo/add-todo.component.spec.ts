099041ffb61ebe3d6af5aa44f2e46e365a1b864b
mike <mike@stuff.com>
2018-05-15 16:31:49 -0600

    updated'

--- src/app/add-todo/add-todo.component.spec.ts
+++ src/app/add-todo/add-todo.component.spec.ts
@@ -1,28 +1,35 @@
 import { async, TestBed, ComponentFixture } from '@angular/core/testing';
 
 import { AddTodoComponent } from './add-todo.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms';
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { defer } from 'rxjs';
 import { TodoService } from '../core/todo.service';
 import { By } from '@angular/platform-browser';
 import { BsModalService } from 'ngx-bootstrap/modal/bs-modal.service';
 import { DebugElement, Predicate } from '@angular/core';
+import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
 
 export function asyncData<T>(data: T) {
   return defer(() => Promise.resolve(data));
 }
 export function getElement<T>(fixture: ComponentFixture<T>, predicate: Predicate<DebugElement>) {
-  return fixture.debugElement.query(By.css('button[type="button"]'));
+  return fixture.debugElement.query(predicate);
 }
 
 describe('AddTodoComponent', () => {
   let component: AddTodoComponent;
   let fixture: ComponentFixture<AddTodoComponent>;
+  let todoService: TodoService;
+  let modalService: BsModalService;
+  let bsModalRef: BsModalRef;
 
-  beforeEach(async(() => {
-    const todoService =  jasmine.createSpyObj('TodoService', ['load']);
-    const modalService = jasmine.createSpyObj('BsModalService', ['show'] );
+
+  beforeEach(() => {
+    bsModalRef = jasmine.createSpyObj('BsModalRef', ['hide']);
+    todoService =  jasmine.createSpyObj('TodoService', ['load', 'add']);
+    modalService = jasmine.createSpyObj('BsModalService', ['show'] );
+    (<any>modalService.show).and.returnValue(bsModalRef);
 
     TestBed.configureTestingModule({
       imports: [
@@ -36,9 +43,7 @@ describe('AddTodoComponent', () => {
       ]
     })
     .compileComponents();
-  }));
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(AddTodoComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -51,11 +56,9 @@ describe('AddTodoComponent', () => {
     expect(component.formGroup).toBeTruthy();
 
     const control = component.formGroup.get('title');
-    expect(control).toBeTruthy();
     expect(control.value).toEqual('');
     expect(control.hasError('required')).toBe(true);
   });
-
   it ('should call openModal on click', () => {
 
     const button = getElement(fixture, By.css('button[type="button"]'));
@@ -64,7 +67,6 @@ describe('AddTodoComponent', () => {
     button.triggerEventHandler('click', null);
     expect(component.openModal).toHaveBeenCalled();
   });
-
   it ('should clear form before opening modal', () => {
     const controlBefore = component.formGroup.get('title');
     controlBefore.setValue('test');
@@ -75,6 +77,17 @@ describe('AddTodoComponent', () => {
     const controlAfter = component.formGroup.get('title');
     expect(controlAfter.value).toEqual('');
 
+  });
+  it ('should add todo when form is valid', () => {
+    const formGroup = new FormGroup({
+      title: new FormControl('')
+    });
+    component.modalRef = bsModalRef;
+
+    component.onSubmit(formGroup);
+
+    expect(todoService.add).toHaveBeenCalled();
+    expect(bsModalRef.hide).toHaveBeenCalled();
   });
 
 });
--- src/app/todo-list/todo-list.component.spec.ts
+++ src/app/todo-list/todo-list.component.spec.ts
@@ -1,25 +1,81 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TodoListComponent } from './todo-list.component';
+import { TodoService } from '../core/todo.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NO_ERRORS_SCHEMA, Predicate, DebugElement } from '@angular/core';
+import { cold, getTestScheduler } from 'jasmine-marbles';
+import { defer } from 'rxjs';
+import { By } from '@angular/platform-browser';
+
+export function asyncData<T>(data: T) {
+  return defer(() => Promise.resolve(data));
+}
+export function getElement<T>(fixture: ComponentFixture<T>, predicate: Predicate<DebugElement>) {
+  return fixture.debugElement.query(predicate);
+}
+export function getAll<T>(fixture: ComponentFixture<T>, predicate: Predicate<DebugElement>) {
+  return fixture.debugElement.queryAll(predicate);
+}
+
 
 describe('TodoListComponent', () => {
   let component: TodoListComponent;
   let fixture: ComponentFixture<TodoListComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ TodoListComponent ]
-    })
-    .compileComponents();
-  }));
+  let todoService: TodoService;
 
   beforeEach(() => {
+    todoService =  new TodoService(null);
+    TestBed.configureTestingModule({
+      imports: [
+        FormsModule,
+        ReactiveFormsModule
+      ],
+      declarations: [ TodoListComponent ],
+      providers: [
+        { provide: TodoService, useValue: todoService }
+      ],
+      schemas: [
+        NO_ERRORS_SCHEMA
+      ]
+    })
+    .compileComponents();
+
     fixture = TestBed.createComponent(TodoListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it ('should initialize observables', () => {
+    
+    const q$ = cold('---x|', []);
+
+    spyOnProperty(todoService, 'todos$', 'get').and.returnValue(q$);
+    
+    expect(component.todos$).toBeTruthy();
+    expect(component.empty$).toBeTruthy();
+
+  });
+
+  it ('should render empty list', () => {
+    const q$ = cold('---x|', []);
+    spyOnProperty(todoService, 'todos$', 'get').and.returnValue(q$);
+
+    getTestScheduler().flush(); // flush the observables
+
+    fixture.detectChanges(); // update view
+
+    const elements = getAll(fixture, By.css('table tr'));
+    expect(elements.length).toBe(2);
+
+    expect(elements[0].nativeElement.innerText).toBe('emtpy');
+    
+  });
+
+
 });

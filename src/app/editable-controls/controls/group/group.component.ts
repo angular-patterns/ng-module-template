b23398b2d16260138734081341147ab33345d0e1
Michael.Kang <michael.kang@gov.ab.ca>
2018-07-18 22:17:34 -0600

    updated

--- src/app/editable-controls/controls/group/group.component.ts
+++ src/app/editable-controls/controls/group/group.component.ts
@@ -2,13 +2,14 @@ import { Component, OnInit, Input, OnChanges, OnDestroy } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 
 import { GroupOptions } from '../../../shared/controls-common/models/group.options';
+import { DropZoneService } from '../../../dynamic/services/drop-zone.service';
 
 
 @Component({
   selector: 'ec-group',
   templateUrl: './group.component.html',
   styleUrls: ['./group.component.css'],
-  providers: [ ]
+  providers: [ DropZoneService ]
 })
 export class GroupComponent implements OnInit {
   @Input() formGroup: FormGroup;
--- src/app/editable-controls/controls/table/table.component.ts
+++ src/app/editable-controls/controls/table/table.component.ts
@@ -1,13 +1,16 @@
 import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { TableOptions } from '../../../shared/controls-common/models/table.options';
+import { WidgetFactory } from '../../../core/widget.factory';
+import { DropZoneService } from '../../../dynamic/services/drop-zone.service';
+import { OptionsDialogService } from '../../../dynamic-options/services/options-dialog.service';
 
 
 @Component({
   selector: 'ec-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.css'],
-  providers: [ ]
+  providers: [ DropZoneService ]
 })
 export class TableComponent implements OnInit, OnChanges {
 
@@ -15,11 +18,35 @@ export class TableComponent implements OnInit, OnChanges {
   @Input() options: TableOptions;
   rows: number[];
   cols: number[];
-  constructor() { 
+  constructor(private widgetFactory: WidgetFactory, private dropZoneService: DropZoneService, private optionsDialogService: OptionsDialogService) { 
     this.rows = [];
     this.cols = [];
     this.formGroup = new FormGroup({});
 
+    this.dropZoneService.drop$.subscribe(t=> {
+      let widget = this.widgetFactory.createWidget(t.widget);
+      let tableWidget = this.findTableWidget(t.args.row, t.args.col);
+      if (tableWidget == null) {
+        tableWidget = { row: t.args.row-1, col: t.args.col-1, widget: null };
+        this.options.widgets.push(tableWidget);
+      }
+      tableWidget.widget = widget;
+      
+    });
+    this.optionsDialogService.remove$.subscribe(o=> {
+      var widgets = this.options.widgets.filter(t=>t.widget != null && t.widget.options == o.options);
+      widgets.forEach(t=> {
+        t.widget = null;
+      });
+    });
+
+    this.optionsDialogService.update$.subscribe(t=> {
+    
+      let index = this.options.widgets.findIndex(x=>x.widget != null && x.widget.options == t.oldOptions);   
+      if (index >= 0) {
+        this.options.widgets[index].widget.options = t.newOptions;
+      } 
+    });
   }
 
   ngOnInit() {
@@ -36,6 +63,9 @@ export class TableComponent implements OnInit, OnChanges {
     let widget = this.options.widgets.find(t=>t.row == i-1 && t.col == j-1);
     return widget ? widget : null;
   }
-
+  tableWidgetExists(i: number, j: number) {
+    let tableWidget = this.findTableWidget(i, j);
+    return tableWidget && tableWidget.widget;
+  }
   
 }

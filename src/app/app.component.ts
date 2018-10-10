b464796cfa40ddded290d520a9db2bf6e04ebd1b
pixelbits <pixelbits100@gmail.com>
2018-10-10 02:03:32 -0600

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {FormGroup} from '@angular/forms';
-import {FormlyFieldConfig, FormlyFormOptions} from '@ngx-formly/core';
+import {FormlyFieldConfig, FormlyFormOptions, FormlyField} from '@ngx-formly/core';
+import { Options } from 'selenium-webdriver/opera';
 
 @Component({
   selector: 'app-root',
@@ -10,56 +11,74 @@ import {FormlyFieldConfig, FormlyFormOptions} from '@ngx-formly/core';
 export class AppComponent {
   form = new FormGroup({});
   model: any = {};
-  options: FormlyFormOptions = {
-    formState: {
-      label: 'test'
-    }
-  };
+  options: FormlyFormOptions = {};
   
-  fields: FormlyFieldConfig[] = [{
-    key: 'highschool',
-    fieldGroup: [
-      {
-        key: 'name',
-        type: 'select',
-        templateOptions: {
-          label: 'Highschool',
-          required: true,
-          options: [
-            { value: 1, label: 'Option 1'  },
-            { value: 2, label: 'Option 2'  },
-            { value: 3, label: 'Option 3'  },
-            { value: 4, label: 'Option 4'  },
-          ]
-        },
-        lifecycle: {
-          onInit: (form, field) => {
-            form.get('name').valueChanges.subscribe(t=> {
-              this.options.formState.label = t;
-            });
+  fields: FormlyFieldConfig[] = [];
+
+  constructor() {
+    this.options = {
+      formState: {
+        options: [
+          { countryId: 'CA', value: 'AB', label: 'Alberta' },
+          { countryId: 'CA', value: 'MB', label: 'Manitoba'},
+          { countryId: 'CA', value: 'SK', label: 'Saskatchewan'},
+          { countryId: 'US', value: 'CA', label: 'California' },
+          { countryId: 'US', value: 'FB', label: 'Florida'},
+          { countryId: 'US', value: 'NJ', label: 'New Jersey'},
+        ]      
+      }
+    };
+    this.fields = [{
+      key: 'address',
+      fieldGroup: [
+        {
+          key: 'countryId',
+          type: 'select',
+          templateOptions: {
+            label: 'Country',
+            required: true,
+            options: [
+              { value: 'CA', label: 'Canada'  },
+              { value: 'US', label: 'USA'  },
+            ]
+          },
+
+          lifecycle: {
+            onInit: (form, field) => {
+              form.get('countryId').valueChanges.subscribe(t=> {
+                this.options.formState.countryId = t;
+              });
+            }
+  
+          },
+          expressionProperties: {
           }
 
-        }
-      },
-      {
-        key: 'other',
-        type: 'input',
-        templateOptions: {
-          label: this.options.formState.label
-          
         },
-        expressionProperties: {
-          'templateOptions.label': (model, formState) => {
-            return this.options.formState.label;
+        {
+          key: 'provinceStateId',
+          type: 'select',
+          templateOptions: {
+            label: 'Province/State',
           },
+          lifecycle: {
+            onInit: (form: FormGroup, field: FormlyFieldConfig) => {
+              
+            }
+          },
+          expressionProperties: {
+            'templateOptions.options': 'formState.options.filter(t=>t.countryId == formState.countryId)',
+          }
+  
         }
-      }
-    ],
-
+      ],
+  
+    }
+  ];
+  
   }
-];
-
   submit(model) {
+    
     console.log(model);
   }
 }

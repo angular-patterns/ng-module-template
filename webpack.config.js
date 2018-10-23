d9a3bcd61efc0be1a8648a83d630f4ae29cb12ce
pixelbits <pixelbits100@gmail.com>
2018-10-23 03:13:25 -0600

    updated

--- webpack.config.js
+++ webpack.config.js
@@ -1,128 +1,20 @@
 const del = require('del');
 const path = require('path');
-const webpack = require('webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const ProgressPlugin = require('webpack/lib/ProgressPlugin');
 const CircularDependencyPlugin = require('circular-dependency-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const autoprefixer = require('autoprefixer');
-const postcssUrl = require('postcss-url');
-const postcssImports = require('postcss-import');
-
 
 const { NoEmitOnErrorsPlugin, NamedModulesPlugin, NormalModuleReplacementPlugin } = require('webpack');
-// const { NamedLazyChunksWebpackPlugin, BaseHrefWebpackPlugin, PostcssCliResources } = require('@angular/cli/plugins/webpack');
-//const { CommonsChunkPlugin } = require('webpack').optimize;
 const { AngularCompilerPlugin } = require('@ngtools/webpack');
 
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-const Dotenv = require('dotenv-webpack');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
-const cssnano = require('cssnano');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
-//const UglifyJsPlugin = require('webpack/lib/optimize/UglifyJsPlugin');
 const UglifyWebpackPlugin = require('uglifyjs-webpack-plugin');
-
-const hashFormat = { "chunk": "", "extract": "", "file": ".[hash:20]", "script": "" };
-const baseHref = process.env.BaseHref;
-const deployUrl = "";
 const projectRoot = process.cwd();
-const maximumInlineSize = 10;
-const postcssPlugins = function (loader) {
-    return [
-        postcssImports({
-            resolve: (url, context) => {
-                return new Promise((resolve, reject) => {
-                    let hadTilde = false;
-                    if (url && url.startsWith('~')) {
-                        url = url.substr(1);
-                        hadTilde = true;
-                    }
-                    loader.resolve(context, (hadTilde ? '' : './') + url, (err, result) => {
-                        if (err) {
-                            if (hadTilde) {
-                                reject(err);
-                                return;
-                            }
-                            loader.resolve(context, url, (err, result) => {
-                                if (err) {
-                                    reject(err);
-                                }
-                                else {
-                                    resolve(result);
-                                }
-                            });
-                        }
-                        else {
-                            resolve(result);
-                        }
-                    });
-                });
-            },
-            load: (filename) => {
-                return new Promise((resolve, reject) => {
-                    loader.fs.readFile(filename, (err, data) => {
-                        if (err) {
-                            reject(err);
-                            return;
-                        }
-                        const content = data.toString();
-                        resolve(content);
-                    });
-                });
-            }
-        }),
-        postcssUrl({
-            filter: ({ url }) => url.startsWith('~'),
-            url: ({ url }) => {
-                const fullPath = path.join(projectRoot, 'node_modules', url.substr(1));
-                return path.relative(loader.context, fullPath).replace(/\\/g, '/');
-            }
-        }),
-        postcssUrl([
-            {
-                // Only convert root relative URLs, which CSS-Loader won't process into require().
-                filter: ({ url }) => url.startsWith('/') && !url.startsWith('//'),
-                url: ({ url }) => {
-                    if (deployUrl.match(/:\/\//) || deployUrl.startsWith('/')) {
-                        // If deployUrl is absolute or root relative, ignore baseHref & use deployUrl as is.
-                        return `${deployUrl.replace(/\/$/, '')}${url}`;
-                    }
-                    else if (baseHref.match(/:\/\//)) {
-                        // If baseHref contains a scheme, include it as is.
-                        return baseHref.replace(/\/$/, '') +
-                            `/${deployUrl}/${url}`.replace(/\/\/+/g, '/');
-                    }
-                    else {
-                        // Join together base-href, deploy-url and the original URL.
-                        // Also dedupe multiple slashes into single ones.
-                        return `/${baseHref}/${deployUrl}/${url}`.replace(/\/\/+/g, '/');
-                    }
-                }
-            },
-            {
-                // TODO: inline .cur if not supporting IE (use browserslist to check)
-                filter: (asset) => {
-                    return maximumInlineSize > 0 && !asset.hash && !asset.absolutePath.endsWith('.cur');
-                },
-                url: 'inline',
-                // NOTE: maxSize is in KB
-                maxSize: maximumInlineSize,
-                fallback: 'rebase',
-            },
-            { url: 'rebase' },
-        ]),
-        // PostcssCliResources({
-        //     deployUrl: loader.loaders[loader.loaderIndex].options.ident == 'extracted' ? '' : deployUrl,
-        //     loader,
-        //     filename: `[name]${hashFormat.file}.[ext]`,
-        // }),
-        autoprefixer({ grid: true }),
-    ];
-};
 
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-require('dotenv').config();
 del.sync("dist/**");
 
 module.exports = (env) => {
@@ -143,17 +35,6 @@ module.exports = (env) => {
     const config = {
         devtool: isOptimized ? false : 'eval-source-map',
         resolve: { extensions: ['.ts', '.js'] },
-        // optimization: {
-        //     splitChunks: {
-        //         // Apply optimizations to all chunks, even initial ones (not just the
-        //         // ones that are lazy-loaded).
-        //         chunks: "all"
-        //     },
-        //     // I pull the Webpack runtime out into its own bundle file so that the
-        //     // contentHash of each subsequent bundle will remain the same as long as the
-        //     // source code of said bundles remain the same.
-        //     runtimeChunk: "single"
-        // },
         optimization: {
             providedExports: true,
             usedExports: true,
@@ -187,7 +68,6 @@ module.exports = (env) => {
                 })],
         },
         entry: {
-            // vendor: './src/vendor.ts',
             polyfills: './src/polyfills.ts',
             app: './src/main.ts',
             styles: './src/styles.css'
@@ -203,6 +83,25 @@ module.exports = (env) => {
                     "test": /\.html$/,
                     "loader": "raw-loader"
                 },
+                {
+                    "exclude": [
+                       path.join(process.cwd(), "src\\styles.css")
+                    ],
+                    "test": /\.css$/,
+                    "use": [
+                        "raw-loader"
+                    ]
+                },         
+                {
+                    "include": [
+                       path.join(process.cwd(), "src\\styles.css")
+                    ],
+                    "test": /\.css$/,
+                    "use": [
+                        "raw-loader",
+                        "css-loader"
+                    ]
+                },                 
                 {
                     "test": /\.(eot|svg|cur)$/,
                     "loader": "file-loader",
@@ -220,100 +119,6 @@ module.exports = (env) => {
                     }
                 },
                 {
-                    "exclude": [
-                        path.join(process.cwd(), "src\\styles.css")
-                    ],
-                    "test": /\.css$/,
-                    "use": [
-                        {
-                            "loader": "raw-loader"
-                        },
-                        {
-                            "loader": "postcss-loader",
-                            "options": {
-                                "ident": "embedded",
-                                "plugins": postcssPlugins,
-                                "sourceMap": true
-                            }
-                        }
-                    ]
-                },
-                {
-                    "exclude": [
-                        path.join(process.cwd(), "src\\styles.css")
-                    ],
-                    "test": /\.scss$|\.sass$/,
-                    "use": [
-                        {
-                            "loader": "raw-loader"
-                        },
-                        {
-                            "loader": "postcss-loader",
-                            "options": {
-                                "ident": "embedded",
-                                "plugins": postcssPlugins,
-                                "sourceMap": true
-                            }
-                        },
-                        {
-                            "loader": "sass-loader",
-                            "options": {
-                                "sourceMap": true,
-                                "precision": 8,
-                                "includePaths": []
-                            }
-                        }
-                    ]
-                },
-                {
-                    "include": [
-                        path.join(process.cwd(), "src\\styles.css")
-                    ],
-                    "test": /\.css$/,
-                    "use": [
-                        "style-loader",
-                        {
-                            "loader": "raw-loader"
-                        },
-                        {
-                            "loader": "postcss-loader",
-                            "options": {
-                                "ident": "embedded",
-                                "plugins": postcssPlugins,
-                                "sourceMap": true
-                            }
-                        }
-                    ]
-                },
-                {
-                    "include": [
-                        path.join(process.cwd(), "src\\styles.css")
-                    ],
-                    "test": /\.scss$|\.sass$/,
-                    "use": [
-                        "style-loader",
-                        {
-                            "loader": "raw-loader"
-                        },
-                        {
-                            "loader": "postcss-loader",
-                            "options": {
-                                "ident": "embedded",
-                                "plugins": postcssPlugins,
-                                "sourceMap": true
-                            }
-                        },
-                        {
-                            "loader": "sass-loader",
-                            "options": {
-                                "sourceMap": true,
-                                "precision": 8,
-                                "includePaths": []
-                            }
-                        }
-                    ]
-                },
-                {
                     test: /(?:\.ngfactory\.js|\.ngstyle\.js|\.ts)$/,
                     loader: '@ngtools/webpack'
                 }
@@ -327,9 +132,6 @@ module.exports = (env) => {
                     resource.request = resource.request.replace(/environment$/, `environment.${nodeEnv}`);
             }),
             new NoEmitOnErrorsPlugin(),
-            new Dotenv({
-                path: './.env'
-            }),
             new CopyWebpackPlugin([
                 {
                     "context": "src",
@@ -354,15 +156,14 @@ module.exports = (env) => {
                         "**/Thumbs.db"
                     ],
                     "debug": "warning"
-                }),
+            }),
             new ProgressPlugin(),
             new CircularDependencyPlugin({
                 "exclude": /(\\|\/)node_modules(\\|\/)/,
-                "failOnError": false,
-                "onDetected": false,
+                "failOnError": true,
+                "onDetected": true,
                 "cwd": projectRoot
             }),
-            //new NamedLazyChunksWebpackPlugin(),
             new HtmlWebpackPlugin({
                 filename: __dirname + '/dist/index.html',
                 template: __dirname + '/src/index.html',
@@ -370,11 +171,6 @@ module.exports = (env) => {
                 compile: true,
                 showErrors: true
             }),
-            //new BaseHrefWebpackPlugin({ baseHref: baseHref }),
-            //new CommonsChunkPlugin({
-            //    names: ['app', 'vendor', 'styles', 'polyfills']
-            //}),
-            new NamedModulesPlugin({}),
             new AngularCompilerPlugin({
                 tsConfigPath: path.join( __dirname, "tsconfig.json" ),
                 entryModule: path.join(__dirname, 'src/app/app.module#AppModule'),
